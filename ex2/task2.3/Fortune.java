71199b13002b33df98e9041aff85d81aae35d93d
vladg94 <vladgeorgescu13@gmail.com>
2015-04-17 02:24:10 +0300

    Erori

---
+++ ex2/task2.3/Fortune.java
@@ -0,0 +1,23 @@
+/**
+*Syntactic errors
+*Class Fortune was about fixing errors
+*First Code
+*public class Fortune
+*{
+*    public static void main[String() args]
+*    {
+*        System.out,println('Sometimes having a fortune is too expensive!);
+*   }
+*}
+*Compiler found 10 errors
+*All of them are syntactic
+*Fixed code below
+*@author Vlad Georgescu
+*/
+public class Fortune
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Sometimes having a fortune is too expensive!");
+    }   
+}
