10791a1670fec7d914d0ffd0f63f0f4f88f5654a
vladg94 <vladgeorgescu13@gmail.com>
2015-04-16 20:10:18 +0300

    Intedare

--- ex1/task1.7/HelloSolarSystem.java
+++ ex1/task1.7/HelloSolarSystem.java
@@ -1,16 +1,15 @@
 public class HelloSolarSystem
 {
-  	public static void main(String[] args)
-  	{
-     	System.out.println("Hello Mercury!");
-     	System.out.println("Hello Venus!");
-     	System.out.println("Hello Earth!");
-     	System.out.println("Hello Mars!");
-     	System.out.println("Hello Jupiter!");
-     	System.out.println("Hello Saturn!");
-     	System.out.println("Hello Uranus!");
-     	System.out.println("Hello Neptune!");
-     	System.out.println("Goodbye Pluto!");
-
-  	}
+    public static void main(String[] args)
+    {
+      System.out.println("Hello Mercury!");
+      System.out.println("Hello Venus!");
+      System.out.println("Hello Earth!");
+      System.out.println("Hello Mars!");
+      System.out.println("Hello Jupiter!");
+      System.out.println("Hello Saturn!");
+      System.out.println("Hello Uranus!");
+      System.out.println("Hello Neptune!");
+      System.out.println("Goodbye Pluto!");
+    }
 }
