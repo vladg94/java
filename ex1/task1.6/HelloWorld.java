ba9cb72ab90fb8d9e3a64646c0bac2fdd4618169
vladg94 <vladgeorgescu13@gmail.com>
2015-04-17 00:42:01 +0300

    final

--- ex1/task1.6/HelloWorld.java
+++ ex1/task1.6/HelloWorld.java
@@ -1,7 +1,11 @@
+/**
+ * Prints 'Hello World'
+ * @author Vlad Georgescu
+ */
 public class HelloWorld 
 {
-	public static void main(String[] args)
-	{
-		System.out.println("HelloWorld!");
-	}
+    public static void main(String[] args)
+    {
+        System.out.println("HelloWorld!");
+    }
 }
--- ex1/task1.7/HelloSolarSystem.java
+++ ex1/task1.7/HelloSolarSystem.java
@@ -1,15 +1,20 @@
+/**
+*Prints Solar System
+*@author Vlad Georgescu
+*/
+
 public class HelloSolarSystem
 {
     public static void main(String[] args)
     {
-      System.out.println("Hello Mercury!");
-      System.out.println("Hello Venus!");
-      System.out.println("Hello Earth!");
-      System.out.println("Hello Mars!");
-      System.out.println("Hello Jupiter!");
-      System.out.println("Hello Saturn!");
-      System.out.println("Hello Uranus!");
-      System.out.println("Hello Neptune!");
-      System.out.println("Goodbye Pluto!");
+        System.out.println("Hello Mercury!");
+        System.out.println("Hello Venus!");
+        System.out.println("Hello Earth!");
+        System.out.println("Hello Mars!");
+        System.out.println("Hello Jupiter!");
+        System.out.println("Hello Saturn!");
+        System.out.println("Hello Uranus!");
+        System.out.println("Hello Neptune!");
+        System.out.println("Goodbye Pluto!");
     }
 }
