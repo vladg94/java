20b850d5c8ec3d6f5fb607dac2d893d0f7d52ebd
Radu Ghitescu <radu.ghitescu@gmail.com>
2015-04-16 17:21:04 +0100

    Fixed tabs

--- Ex1/Task1.7/HelloSolarSystem.scala
+++ Ex1/Task1.7/HelloSolarSystem.scala
@@ -3,15 +3,15 @@
  * except Pluto
  */
 object HelloSolarSystem {
-	def main(args: Array[String]) {
-		println("Hello, Mercury!")
-		println("Hello, Venus!")
-		println("Hello, Earth!")
-		println("Hello, Mars!")
-		println("Hello, Jupiter!")
-		println("Hello, Saturn!")
-		println("Hello, Uranus!")
-		println("Hello, Neptune!")
-		println("Goodbye, Pluto!")
-	}
+    def main(args: Array[String]) {
+        println("Hello, Mercury!")
+        println("Hello, Venus!")
+        println("Hello, Earth!")
+        println("Hello, Mars!")
+        println("Hello, Jupiter!")
+        println("Hello, Saturn!")
+        println("Hello, Uranus!")
+        println("Hello, Neptune!")
+        println("Goodbye, Pluto!")
+    }
 }
