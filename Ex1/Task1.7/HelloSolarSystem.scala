a88a563e74e2d02040fb311709db5fb414728e02
Radu Ghitescu <radu.ghitescu@gmail.com>
2015-04-16 16:58:15 +0100

    Ex 1.7

---
+++ Ex1/Task1.7/HelloSolarSystem.scala
@@ -0,0 +1,17 @@
+/**
+ * Says hello to every planet in the Solar System
+ * except Pluto
+ */
+object HelloSolarSystem {
+	def main(args: Array[String]) {
+		println("Hello, Mercury!")
+		println("Hello, Venus!")
+		println("Hello, Earth!")
+		println("Hello, Mars!")
+		println("Hello, Jupiter!")
+		println("Hello, Saturn!")
+		println("Hello, Uranus!")
+		println("Hello, Neptune!")
+		println("Goodbye, Pluto!")
+	}
+}
