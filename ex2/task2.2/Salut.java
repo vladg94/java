fa7086dc996a0e56f7778d214a1f77066306bda2
vladg94 <vladgeorgescu13@gmail.com>
2015-04-17 01:57:59 +0300

    Cap2

---
+++ ex2/task2.2/Salut.java
@@ -0,0 +1,11 @@
+/*
+*Prints smth in romanian
+*@author Vlad Georgescu 
+*/
+public class Salut
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Salut");
+    }    		
+}
