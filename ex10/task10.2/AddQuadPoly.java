8629b1b06af9f4bbb110683fdad8ba368d502317
vladg94 <vladgeorgescu13@gmail.com>
2015-05-09 20:16:25 +0300

    NO ERRORS MTF

---
+++ ex10/task10.2/AddQuadPoly.java
@@ -0,0 +1,44 @@
+/**
+* Prints sum of 2 quadratic polynomials
+* Tested for
+* @author Vlad Georgescu 
+*/
+public class AddQuadPoly 
+{   
+    public static void main(String[] args) 
+    {
+        //Creating objects
+        //Storing them into clases variables
+        QuadPoly firstPoly = new QuadPoly(Double.parseDouble(args[0]) 
+            , Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+        QuadPoly secondPoly = new QuadPoly(Double.parseDouble(args[3]) 
+            , Double.parseDouble(args[4]), Double.parseDouble(args[5]));
+
+        //Storing the sum of every coeficient into another class variable
+        QuadPoly sumOfPolys = new QuadPoly(Double.parseDouble(args[0]) 
+            + Double.parseDouble(args[3]), Double.parseDouble(args[1]) 
+            + Double.parseDouble(args[4]), Double.parseDouble(args[2]) 
+            + Double.parseDouble(args[5]));
+        
+        //Printing quadratic polynomials 
+        //First one
+        System.out.print("Polynomyal:    ");
+        AddQuadPoly.printPoly(firstPoly.firstCoef, firstPoly.secondCoef, 
+            firstPoly.thirdCoef);
+
+        //Second one
+        System.out.print("added to:      ");
+        AddQuadPoly.printPoly(secondPoly.firstCoef, secondPoly.secondCoef,
+            secondPoly.thirdCoef);
+
+        //The sum
+        System.out.print("result is:     ");
+        AddQuadPoly.printPoly(sumOfPolys.firstCoef, sumOfPolys.secondCoef, 
+            sumOfPolys.thirdCoef);
+    }
+
+    private static void printPoly (double a, double b, double c)
+    {
+        System.out.println(a + "x^2 + " + b + "x + " + c);
+    }
+}
---
+++ ex10/task10.2/QuadPoly.java
@@ -0,0 +1,17 @@
+/**
+* Template class
+* @author Vlad Georgescu
+*/
+public class QuadPoly 
+{
+    public double firstCoef;
+    public double secondCoef;
+    public double thirdCoef;
+
+    QuadPoly (double requieredSt, double requieredNd,double requieredTh) 
+    {
+        firstCoef = requieredSt;
+        secondCoef = requieredNd;
+        thirdCoef = requieredTh;
+    }
+}
