5b78924f388be462739f5a2c0f8268587b74c7b3
vladg94 <vladgeorgescu13@gmail.com>
2015-05-12 04:27:20 +0300

    vladGGG

---
+++ ex10/task10.3/CompareQuadPoly.java
@@ -0,0 +1,25 @@
+/**
+* Prints comparision bewteen two polynomials
+* Tested for 2 3 4 3 4 5 
+* @author Vlad Georgescu
+*/
+public class CompareQuadPoly
+{
+    public static void main(String[] args)
+    {
+        QuadPoly firstPoly = new QuadPoly(Integer.parseInt(args[0]), 
+            Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        QuadPoly secondPoly = new QuadPoly(Integer.parseInt(args[3]),
+            Integer.parseInt(args[4]), Integer.parseInt(args[5]));
+
+        if (firstPoly.equals(secondPoly)) {
+            System.out.println("The two polynomials are the same");
+        }
+
+        if (firstPoly.lessThan(secondPoly)) {
+            System.out.println("The first polynomial is less than the second");
+        } else {
+            System.out.println("AM PULA MARE");
+        }
+    }
+}
---
+++ ex10/task10.3/QuadPoly.java
@@ -0,0 +1,29 @@
+/**
+* QuadPoly class
+* @author Vlad Georgescu
+*/
+public class QuadPoly 
+{
+    public double firstCoef;
+    public double secondCoef;
+    public double thirdCoef;
+
+    public QuadPoly(double firstCoef, double secondCoef,double thirdCoef) 
+    {
+        this.firstCoef = firstCoef;
+        this.secondCoef = secondCoef;
+        this.thirdCoef = thirdCoef;
+    }
+
+    public boolean equals(QuadPoly other)
+    {
+        return firstCoef == other.firstCoef && secondCoef == other.secondCoef
+            && thirdCoef == other.thirdCoef;
+    }
+
+    public boolean lessThan(QuadPoly other)
+    {
+        return firstCoef < other.firstCoef || secondCoef < other.secondCoef
+        || thirdCoef < other.thirdCoef;
+    }
+}
